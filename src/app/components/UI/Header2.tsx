722d656816714f1438fd431f90a182bfba282e19
Russs11 <russs.khalilov@gmail.com>
2025-09-18 23:57:06 +0300

    h18.09.2025

--- src/app/components/UI/Header2.tsx
+++ src/app/components/UI/Header2.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { siteConfig } from '@/app/config/site.config'
 import {
   Navbar,
   NavbarBrand,
@@ -8,34 +7,17 @@ import {
   Button,
 } from '@heroui/react'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 
 
 export default function Header() {
-const pathname = usePathname()
 
-const getNavItems = () => {
-return siteConfig.navItems.map(item => {
-    const isActive = pathname === item.href
-    return (
-    <NavbarItem className='justify-center' key={item.href}>
-        <Link
-        color='foregound'
-        href={item.href}
-        className={`px-3 py-1 ${
-            isActive ? 'text-blue-500' : 'text-foreground'
-        } font-bold`}
-        >
-        {item.label}
-        </Link>
-    </NavbarItem>
-    )
-})
-}
 
 return (
     <>
-    <Navbar>
+    <Navbar isBordered>
+        <NavbarBrand>
+        <p className="font-bold text-md text-blue-600">Форма о фактах происшествий</p>
+        </NavbarBrand>
         <NavbarContent justify='end'>
         <NavbarItem className='hidden lg:flex'>
         </NavbarItem>
@@ -44,14 +26,6 @@ return (
         Войти     
             </Button>
         </NavbarItem>
-        </NavbarContent>
-    </Navbar>
-    <Navbar isBordered>
-        <NavbarContent className='hidden sm:flex gap-4'>
-        {getNavItems()}
-        </NavbarContent>
-        <NavbarContent className='hidden sm:flex gap-4'>
-        {getNavItems()}
         </NavbarContent>
     </Navbar>
     </>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Geist, Geist_Mono, Inter } from 'next/font/google'
 import './globals.css'
 import Header from './components/UI/Header2'
 import { Providers } from '@/providers/providers'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+
+
+const inter = Inter({
+  variable: '--font-inter-sans',
   subsets: ['latin'],
 })
 
@@ -25,9 +27,9 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
+    <html className="light " lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased` }
+        className={`light ${inter.variable} antialiased` }
       >
         <Providers >
           <Header/>
