e18afad8a6cd7621a58037c81925e9348ef6a207
Russs11 <russs.khalilov@gmail.com>
2025-09-19 17:26:54 +0300

    w19.09.2025

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono, Inter } from 'next/font/google'
 import './globals.css'
-import Header from './components/UI/Header'
+import Header from './components/UI/Header2'
 import { Providers } from '@/providers/providers'
 
 
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html className="light " lang='en'>
+    <html className="light" lang='en'>
       <body
         className={`light ${inter.variable} antialiased` }
       >
