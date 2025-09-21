f3a0fbb3864302775149e00a49bf3006445ed997
Russs11 <russs.khalilov@gmail.com>
2025-09-21 20:13:51 +0300

    h21.09.2025

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono, Inter } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from '@/providers/providers'
-import HeaderWithLinks from './components/Header'
 import Header from './components/Header'
 
 
@@ -12,10 +11,6 @@ const inter = Inter({
   subsets: ['latin'],
 })
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
 
 export const metadata: Metadata = {
   title: 'Форма о фактах происшествий'
