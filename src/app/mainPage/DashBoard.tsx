ba8e38c18214d234f9e21b08f8f2f03b71294dd8
Russs11 <russs.khalilov@gmail.com>
2025-09-25 22:34:55 +0300

    h25.09.2025

--- src/app/mainPage/DashBoard.tsx
+++ src/app/mainPage/DashBoard.tsx
@@ -1,8 +1,6 @@
 'use client'
 
-import { Avatar } from "@heroui/react";
 import TabMenu from "../components/UI/TabMenu";
-import UserAvatar from "../components/UI/UserAvatar";
 
 
 export function Dashboard() {
@@ -10,7 +8,6 @@ export function Dashboard() {
         <>
             <div className='p-8'>
                 <TabMenu />
-                <UserAvatar />
             </div>
         </>
     );
