c31a4b5e1dc5590b47819c0ef5d341f6cc19e1b9
Russs11 <russs.khalilov@gmail.com>
2025-09-28 23:36:56 +0300

    h28.09.2025

--- src/app/mainPage/DashBoard.tsx
+++ src/app/mainPage/DashBoard.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Button } from "@heroui/react";
 import DropdownComponent from "../components/UI/DropdownComponent";
 import TabMenu from "../components/UI/TabMenu";
 
@@ -8,10 +9,11 @@ export function Dashboard() {
     return (
         <>
             <div className='grid max-h-max p-10 2xl:grid-rows-2 '>
-                <div className='grid grid-cols-1 gap-10 justify-self-end px-10 2xl:grid-cols-3 2xl:max-w-3/4 '>
+                <div className='grid grid-cols-1 gap-10 justify-self-end px-10 2xl:grid-cols-4 2xl:max-w-3/4 '>
                     <DropdownComponent />
                     <DropdownComponent />
                     <DropdownComponent />
+
                 </div>
                 <div className='grid absolute w-screen 2xl:block '>
                 <TabMenu />
