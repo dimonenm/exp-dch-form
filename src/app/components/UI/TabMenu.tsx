c82d0a93a110e9d6d60b4e99e525310b55c3e35e
Russs11 <russs.khalilov@gmail.com>
2025-10-02 22:55:01 +0300

    h02.10.2025

--- src/app/components/UI/TabMenu.tsx
+++ src/app/components/UI/TabMenu.tsx
@@ -1,5 +1,6 @@
 
 import { Tabs, Tab, Card, CardBody, Input } from "@heroui/react";
+import TableCard from "../TableCard";
 
 export default function TabMenu() {
     const tabs = [
@@ -30,7 +31,14 @@ export default function TabMenu() {
                 {(item) => (
                     <Tab key={item.id} title={item.label}>
                         <Card className='mt-8 w-[95%]'>
-                            <CardBody className='grid grid-cols-4 gap-4'>
+                            <CardBody className='grid grid-cols-[1fr_1fr_2fr] gap-4'>
+                                <TableCard />
+                                <TableCard />
+                                <TableCard />
+                            </CardBody>
+                        </Card>
+                        <Card className='mt-8 w-[95%]'>
+                            <CardBody className='grid grid-cols-3 gap-4'>
                                 {/* {item.content} */}
                                 <Input />
                                 <Input />
@@ -38,15 +46,7 @@ export default function TabMenu() {
                             </CardBody>
                         </Card>
                         <Card className='mt-8 w-[95%]'>
-                            <CardBody className='grid grid-cols-4 gap-4'>
-                                {/* {item.content} */}
-                                <Input />
-                                <Input />
-                                <Input />
-                            </CardBody>
-                        </Card>
-                        <Card className='mt-8 w-[95%]'>
-                            <CardBody className='grid grid-cols-4 gap-4'>
+                            <CardBody className='grid grid-cols-3 gap-4'>
                                 {/* {item.content} */}
                                 <Input />
                                 <Input />
